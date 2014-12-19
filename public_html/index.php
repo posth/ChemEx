3eab4524e1ec77a61a877724678cd8f259d10c89
posth <posth.marc@gmail.com>
2014-12-19 13:17:28 -0500

    ChemEx
    Latest code version of the site.

---
+++ public_html/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$default_language = "en";
+$file = "index.html";
+
+if (($_SERVER["HTTP_ACCEPT_LANGUAGE"] != "") && ($_SERVER["HTTP_ACCEPT_LANGUAGE"] != $default_language)) {
+   $redirects = array(
+      $_SERVER["HTTP_ACCEPT_LANGUAGE"] . "/" . $file,
+      substr($_SERVER["HTTP_ACCEPT_LANGUAGE"], 0, 2) . "/" . $file,
+   );
+   foreach ($redirects as $redirect) {
+      if(@file_exists($redirect)) {
+         header("Location: ".$redirect);
+         exit;
+      }
+   }
+} 
+
+// No index for specific language specified
+header("Location: ".$default_language."/".$file);  
